1e91b95ef01cc85bf8f6a40b3f1ac27f87c5eb3e
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-08-17 16:47:50 +0100

    add functionality

--- src/state/store.js
+++ src/state/store.js
@@ -2,7 +2,11 @@ import { createStore, applyMiddleware } from 'redux'
 import reducers from "./reducers"
 import thunk from 'redux-thunk';
 
-export const store = createStore(
+export 
+
+
+
+const store = createStore(
   reducers,
     {},
     applyMiddleware(thunk)
